2f1ed60d71583ce63e958f65d2053fd861da96f4
blue-stone <qingshiji96@gmail.com>
2023-12-31 14:33:09 +0800

    init

---
+++ factory_calib/include/round_hole_board/lidar_pattern.h
@@ -0,0 +1,19 @@
+#include <eigen3/Eigen/Dense>
+
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace lidarcalib
+{
+
+class LidarDetector
+{
+ public:
+  LidarDetector( ){ };
+  ~LidarDetector( ){ };
+
+  void LidarDetection(std::string pcds_dir);
+};
+
+} // namespace lidarcalib
